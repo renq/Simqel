3cd3e82be55a2e6b45205acf91040345c2c4440f
Michał Lipek <michal@lipek.net>
2013-04-10 00:10:48 +0200

    PDO::quote - fix if parameter is array

--- src/Simqel/Connection/PDO.php
+++ src/Simqel/Connection/PDO.php
@@ -4,37 +4,37 @@ namespace Simqel;
 
 
 /**
- * 
+ *
  * Enter description here ...
  * @author Michał Lipek (michal@lipek.net)
  *
  */
 abstract class Connection_PDO extends BaseConnection {
-	
+
 	protected $handle = null;
 	private $lastStatement = null;
-	
-	
+
+
 	public function __construct(Settings $settings) {
 		parent::__construct($settings);
 	}
-	
-	
+
+
 	public function disconnect() {
 		$this->handle = null;
 	}
-	
-	
+
+
 	public function getHandle() {
 		return $this->handle;
 	}
-	
-	
+
+
 	public function setHandle($handle) {
 		$this->handle = $handle;
 	}
-	
-	
+
+
 	public function escape($value) {
 		$this->connect();
 		if (is_null($value)) {
@@ -49,12 +49,17 @@ abstract class Connection_PDO extends BaseConnection {
 		elseif (is_bool($value)) {
 			return (string)((int)$value);
 		}
+		elseif (is_array($value)) {
+		    foreach ($value as &$v) {
+		        $v = $this->escape($v);
+		    }
+		}
 		else {
 			return $this->handle->quote($value);
 		}
 	}
-	
-	
+
+
 	public function query($query, array $params = array()) {
 		$this->connect();
 		try {
@@ -73,8 +78,8 @@ abstract class Connection_PDO extends BaseConnection {
 		$this->lastStatement = $sth;
 		return $sth;
 	}
-	
-	
+
+
 	public function getAffectedRows() {
 		if ($this->lastStatement instanceof \PDOStatement) {
 			return $this->lastStatement->rowCount();
@@ -83,38 +88,38 @@ abstract class Connection_PDO extends BaseConnection {
 			throw new Exception("No query was executed, so method getRowsAffected is pointless in this moment.");
 		}
 	}
-	
-	
+
+
 	public function lastInsertId($table = '', $idColumn = '') {
 		$this->connect();
 		return $this->handle->lastInsertId();
 	}
 
-	
+
 	public function fetch($sth) {
 		$this->connect();
 		return $sth->fetch(\PDO::FETCH_ASSOC);
 	}
-	
-	
+
+
 	public function beginTransaction() {
 		$this->connect();
 		$this->handle->beginTransaction();
 	}
-	
-	
+
+
 	public function commit() {
 		$this->connect();
 		$this->handle->commit();
 	}
-	
-	
+
+
 	public function rollback() {
 		$this->connect();
 		$this->handle->rollback();
 	}
-	
-	
-} 
+
+
+}
 
 
